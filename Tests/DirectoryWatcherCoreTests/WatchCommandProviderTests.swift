b0d708b782383e69b42441b1cf6aad5017672366
Matt Gacy <matt@mgacy.com>
2023-07-06 09:51:03 -0700

    Test WatchCommandProvider

---
+++ Tests/DirectoryWatcherCoreTests/WatchCommandProviderTests.swift
@@ -0,0 +1,39 @@
+//
+//  WatchCommandProviderTests.swift
+//  DirectoryWatcher
+//
+//  Created by Mathew Gacy on 6/24/23.
+//
+
+@testable import DirectoryWatcherCore
+import Foundation
+import XCTest
+
+final class WatchCommandProviderTests: XCTestCase {
+    func testCommandForWatchedFile() throws {
+        let (executor, provider) = WatchCommandProvider.mock(watching: Mock.watchedPath)
+        let config = Mock.commandConfigurations[0]
+        let command = try provider.watchCommand(config)
+
+        command(Mock.matchingSwiftEvent)
+        XCTAssertEqual(executor.executedCommands, [config.command])
+    }
+
+    func testUnwatchedFileIgnored() throws {
+        let (executor, provider) = WatchCommandProvider.mock(watching: Mock.watchedPath)
+        let config = Mock.commandConfigurations[0]
+        let command = try provider.watchCommand(config)
+
+        command(Mock.notMatchingEvent)
+        XCTAssertEqual(executor.executedCommands, [])
+    }
+
+    func testExcludedFileIgnored() throws {
+        let (executor, provider) = WatchCommandProvider.mock(watching: Mock.watchedPath)
+        let config = Mock.commandConfigurations[1]
+        let command = try provider.watchCommand(config)
+
+        command(Mock.excludedEvent)
+        XCTAssertEqual(executor.executedCommands, [])
+    }
+}
