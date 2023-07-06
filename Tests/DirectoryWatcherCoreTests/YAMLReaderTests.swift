4c0443e506e941ad758a9ae9fbd38fd0e225a36f
Matt Gacy <matt@mgacy.com>
2023-07-06 09:51:26 -0700

    Test YAMLReader

---
+++ Tests/DirectoryWatcherCoreTests/YAMLReaderTests.swift
@@ -0,0 +1,35 @@
+//
+//  YAMLReaderTests.swift
+//  DirectoryWatcher
+//
+//  Created by Mathew Gacy on 6/24/23.
+//
+
+@testable import DirectoryWatcherCore
+import Foundation
+import XCTest
+
+final class YAMLReaderTests: XCTest {
+    let configURL = URL(fileURLWithPath: "\(Mock.commandConfigurations)\(".watcher.yml")")
+
+    func testReadValidConfig() throws {
+        var readURL: URL?
+        let sut = YAMLReader(read: { url in
+            readURL = url
+            return Mock.configYAML
+        })
+
+        let actual: [CommandConfiguration]? = try sut.read(at: configURL)
+
+        XCTAssertEqual(actual, Mock.commandConfigurations)
+        XCTAssertEqual(readURL, configURL)
+    }
+
+    func testReadInvalidValidConfig() throws {
+        let sut = YAMLReader(read: { _ in "foo" })
+        XCTAssertThrowsError({
+            let config: [CommandConfiguration]? = try sut.read(at: self.configURL)
+            _ = config
+        })
+    }
+}
